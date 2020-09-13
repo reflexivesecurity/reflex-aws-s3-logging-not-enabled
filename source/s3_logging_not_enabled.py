dc3a76b377080d9f0d572cb6c4965c79a4050fc7
rjulian <richard@rjulian.net>
2020-09-13 14:45:59 -0700

    Semi-automated multi account push

--- source/s3_logging_not_enabled.py
+++ source/s3_logging_not_enabled.py
@@ -4,7 +4,7 @@ import json
 import os
 
 import boto3
-from reflex_core import AWSRule
+from reflex_core import AWSRule, subscription_confirmation
 
 
 class S3LoggingNotEnabled(AWSRule):
@@ -43,5 +43,8 @@ class S3LoggingNotEnabled(AWSRule):
 def lambda_handler(event, _):
     """ Handles the incoming event """
     print(event)
+    if subscription_confirmation.is_subscription_confirmation(event):
+        subscription_confirmation.confirm_subscription(event)
+        return
     s3_rule = S3LoggingNotEnabled(json.loads(event["Records"][0]["body"]))
     s3_rule.run_compliance_rule()
